3ff60928d7914529a4074d63219b74ba0f0315b5
rahulkumarbudhwani1 <rahulkumarbudhwani@gmail.com>
2020-12-12 16:21:36 +0100

    first commit

--- include/kitty/threshold_identification.hpp
+++ include/kitty/threshold_identification.hpp
@@ -35,7 +35,8 @@
 #include <vector>
 // #include <lpsolve/lp_lib.h> /* uncomment this line to include lp_solve */
 #include "traits.hpp"
-
+#include <fstream>
+#include <iostream>
 namespace kitty
 {
 
@@ -58,19 +59,173 @@ namespace kitty
 template<typename TT, typename = std::enable_if_t<is_complete_truth_table<TT>::value>>
 bool is_threshold( const TT& tt, std::vector<int64_t>* plf = nullptr )
 {
-  std::vector<int64_t> linear_form;
+	
+	std::cout << "num vars is " <<tt.num_vars() << std::endl;
+	
+	//auto bit_iter = tt.begin();
+	
+	int num_vars = tt.num_vars();
+	std::vector<int64_t> linear_form = std::vector<int64_t> (num_vars+1);
+	uint64_t base = 1u;
+	
+	std::vector<int> tracker = std::vector<int>(num_vars); 
+	int comp = 1u << tt.num_vars();
+	
+	TT new_tt= tt;
+	auto new_bit_iter = new_tt.begin();
+	std::cout << "bits is " << *new_bit_iter << std::endl;
+	
+	for(int i=0 ; i< num_vars ; i++){
+		int temp=0; int temp_temp= 0;
+		int jump = int(1u << (i+1));
+			
+		for(int j=0; j < comp ; j = j + jump){
+				
+			for(int k=0; k< int(1u << i) ; k++) {
+					
+				uint64_t val1,val2;
+				val1= *(new_bit_iter+((j+k)/64));
+				val2= *(new_bit_iter+((j+k+int(1u << i))/64));
+					
+				uint64_t bit1 = (((val1 & ( base << ((j+k)%64) )) >> ((j+k)%64)) ) ;
+				uint64_t bit2 = (((val2 & ( base << ((j+k+int(1u << i))%64) )) >> ((j+k+int(1u << i))%64)));
+				int diff = int(bit2) - int(bit1);
+					
+				if(  diff == 1  || diff == -1)
+					temp_temp = diff;	
+						
+						
+				if(   (temp ==1 && temp_temp == -1) || (temp ==-1 && temp_temp == 1) ){
+					//std::cout << "it is " << false<< std::endl;
+					return false;
+				}
+					
+				if(temp_temp != 0)
+					temp = temp_temp;
+						
+					//std::cout << j+k <<" " <<(j+k+int(1u << i))<< std::endl;
+			}
+		}
+		
+		
+		//std::cout << "temp is " << temp<< std::endl;
+		if(temp==-1){
+			tracker.at(i)=1;
+			for(int j=0; j < comp ; j = j + jump){
+				
+				for(int k=0; k< int(1u << i) ; k++) {
+					
+					uint64_t val1,val2;
+					val1= *(new_bit_iter+((j+k)/64));
+					val2= *(new_bit_iter+((j+k+int(1u << i))/64));
+					
+					uint64_t bit1 = (((val1 & ( base << ((j+k)%64) )) >> ((j+k)%64))  << ((j+k+int(1u << i))%64)) ;
+					uint64_t bit2 = (((val2 & ( base << ((j+k+int(1u << i))%64) )) >> ((j+k+int(1u << i))%64)) << ((j+k)%64));
+					
+					*(new_bit_iter+((j+k)/64)) = (( *(new_bit_iter+((j+k)/64)) & (~( base << ((j+k)%64) )) ) | bit2);
+					*(new_bit_iter+((j+k+int(1u << i))/64)) = (( *(new_bit_iter+((j+k+int(1u << i))/64)) & (~( base << ((j+k+int(1u << i))%64) )) ) | bit1);
+					
+				}
+			}
+		}
+	}
+	
+	
+	std::string filename = "scheduling.lp" ;
+	std::ofstream fout( filename, std::ofstream::out );
+	fout << "min:";
+	for ( int l = 0; l < num_vars; ++l )
+    {
+      fout << " + " << "w" << "_" << l;
+    }
+    fout << " + " << "T" << " ;" << std::endl;
+	for ( int l = 0; l < num_vars; ++l )
+    {
+      fout  << "w" << "_" << l <<" >= " << "0 ;" << std::endl;
+    }
+    fout  << "T" <<" >= " << "0 ;" << std::endl;
+	
+	for(int i=0 ; i< comp ; i++){
+		
+		int bit1 = int((*(new_bit_iter+(i/64)) & ( base << ((i)%64) )) >> ((i)%64));
+		for(int j=0 ; j< num_vars ; j++){
+			int bit2 = (i / (int( base << j)))% 2 ;
+			if(bit2 == 1){
+					fout  << "w" << "_" << j  << " + " ;
+			}
+			
+		}
+		if(bit1 == 1){
+			fout << " 0 " << " >= " << "T ;" << std::endl;
+		}
+		if(bit1 == 0){
+			fout << " 0 " << " <= " << "T - 1 ;" << std::endl;
+		}
+	}
+	std::system( "lp_solve scheduling.lp > solution.txt" );
+	
+	std::vector<int64_t> weight = std::vector<int64_t>(num_vars+1);
+	
+	std::ifstream fin( "solution.txt", std::ifstream::in );
+	if ( !fin.is_open() )
+    {
+      std::cerr << "[e] Error opening the solution file (dump of lp_solve print-out)." << std::endl;
+      
+    }
+	
+	std::string line, obj;
+    std::getline( fin, line ); /* first line is empty */
+	if(line[0]=='T')
+		return false;
+    std::getline( fin, obj ); /* second line is the value of objective function */
+	
+    std::getline( fin, line ); /* third line is empty */
+    std::getline( fin, line ); /* fourth line is useless */
+	for(int i=0; i< num_vars ; i++){
+	
+		std::getline( fin, line );
+		std::string var_name, value = "";
+		std::stringstream ss( line );
+		std::getline( ss, var_name, ' ' );
+		std::getline( ss, value );
+		//std::cout << var_name << " " << value << std::endl;
+		linear_form.at( std::stoi(var_name.erase( 0, 2 ))) = std::stoi(value);
+		//while ( value.size() == 0u && std::getline( ss, value, ' ' ) ) { }
+		//if ( value[0] == '1' )
+		//{
+		//	std::string str_i, str_l;
+		//	std::stringstream ss2( var_name.erase( 0, 2 ) );
+		//	std::getline( ss2, str_l );
+		//	weight.at( std::stoi( str_l ) ) = 1;
+		//}
+    }
+	
+	std::getline( fin, line );
+	std::string var_name, value = "";
+	std::stringstream ss( line );
+	std::getline( ss, var_name, ' ' );
+	std::getline( ss, value );
+	//std::cout << var_name << " " << value << std::endl;
+	linear_form.at( num_vars ) = std::stoi(value);
+		
 
-  /* TODO */
-  /* if tt is non-TF: */
-  return false;
+	//std::cout << "bits is " << *new_bit_iter << std::endl;
+	//std::cout << "it is " << true<< std::endl;
+	for(int i=0;i<num_vars;i++){
+		if(tracker.at(i)==1){
+			linear_form.at(i)=-linear_form.at(i);
+			linear_form.at(num_vars) =  linear_form.at(num_vars) + linear_form.at(i);
+		}
+	}
+
 
   /* if tt is TF: */
   /* push the weight and threshold values into `linear_form` */
-  if ( plf )
-  {
-    *plf = linear_form;
-  }
-  return true;
+	if ( plf )
+	{
+		*plf = linear_form;
+	}
+	return true;
 }
 
 } /* namespace kitty */
